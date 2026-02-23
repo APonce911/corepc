c6d6ccd2e8faf821483e8bbb4c0a06e4cbd6bfdb
airton <ponceairton@gmail.com>
2026-02-23 23:49:30 +0100

    format adjustment

--- bitreq/src/connection/certificates.rs
+++ bitreq/src/connection/certificates.rs
@@ -1,11 +1,10 @@
+#[cfg(feature = "rustls")]
+use std::sync::Arc;
 #[cfg(all(feature = "native-tls", not(feature = "rustls"), feature = "tokio-native-tls"))]
 use std::sync::{Arc, Mutex};
 
 #[cfg(all(feature = "native-tls", not(feature = "rustls")))]
 use native_tls::{Certificate, TlsConnector, TlsConnectorBuilder};
-#[cfg(feature = "rustls")]
-use std::sync::Arc;
-
 #[cfg(feature = "rustls")]
 use rustls::RootCertStore;
 #[cfg(feature = "rustls-webpki")]
